3aae6a8f8651071af6a6a2b9997756b031feb425
yash-nomadic <yashsingh4537@gmail.com>
2025-02-01 13:44:07 +0530

    using jsx

---
+++ src/app/page.jsx
@@ -0,0 +1,19 @@
+ import React from 'react'
+ 
+ const Home = () => {
+   return (
+    <div>
+
+
+      <h1 className='text-center font-bold my-5 text-3xl'>Home</h1>
+
+      <h2 style={{ color:'red', textAlign:'center', fontSize: 30  }}>Welcome to React using Next JS</h2>
+
+      <input type="text" className='border border-gray-800 p-2' placeholder='text' />
+     
+     
+    </div>
+   )
+ }
+ 
+ export default Home;
---
+++ src/app/signup/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+        <h1 className='text-center font-bold my-10 text-3xl'>Signup</h1>
+        </div>
+  )
+}
+
+export default Signup;
