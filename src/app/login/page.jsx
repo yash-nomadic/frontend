33419c935f6cb47f173756845d3dac1b887447e4
yash-nomadic <yashsingh4537@gmail.com>
2025-02-01 18:23:12 +0530

    login page in next js

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -20,7 +20,7 @@ const Login = () => {
           <label htmlFor="pasword">Password</label><br />
           <input type="password" placeholder='Enter your password here' className='border border-gray-800 w-full ' /><br />
 
-          <button className='border w-1/2  rounded-lg bg-blue-500 text-white text-center  '>Submit</button>
+          <button className='border w-1/2  rounded-lg bg-blue-500 text-white text-center'>Submit</button>
 
 
         </form>
