86ee4a7702a92eba166b412cd8808e4f866535f6
yash-nomadic <yashsingh4537@gmail.com>
2025-02-01 17:47:26 +0530

    login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,11 +2,36 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-        <h1 className='text-center font-bold my-10 text-3xl'>Login</h1>
 
-        <button className='global-btn'>Login Button</button>
+
+
+    <div className='border w-1/5 border-gray-800 align center rounded-lg shadow-gray-800  '>
+
+      <h1 className='text-center font-bold my-10 text-3xl'>Login</h1>
+
+      <div>
+
+        <form className='text-xl p-8 '>
+
+          <label htmlFor="email" >Email</label><br />
+          <input type="text" placeholder='Enter your email here' className='border border-gray-800 w-full ' /><br />
+
+
+          <label htmlFor="pasword">Password</label><br />
+          <input type="password" placeholder='Enter your password here' className='border border-gray-800 w-full ' /><br />
+
+          <button className='border w-1/2  rounded-lg bg-blue-500 text-white text-center  '>Submit</button>
+
+
+        </form>
+
+
+      </div>
+
     </div>
+
+
+
   )
 }
 
