23846081cc7c5bcdaa7967b0d09b20b30545c2f1
yash-nomadic <yashsingh4537@gmail.com>
2025-02-01 17:47:40 +0530

    sign up

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>
-        <h1 className='text-center font-bold my-10 text-3xl'>Signup</h1>
-        </div>
+     
+    
+
+      <div>
+
+
+
+          
+
+      </div>
+
+
+
+
+
+
+
+    
   )
 }
 
