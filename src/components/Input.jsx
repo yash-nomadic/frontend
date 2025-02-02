f9fb95ee6beb5a07ba9551f6d661964eae46dec7
yash-nomadic <yashsingh4537@gmail.com>
2025-02-02 21:50:20 +0530

    input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Input = ({ labelname, }) => {
+  return (
+    <div>
+
+       <label htmlFor="">Name</label>
+       <input type={"type"} placeholder='Enter your naame'/> 
+
+    </div>
+  )
+}
+
+export default Input;
