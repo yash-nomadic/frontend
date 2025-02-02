b3a01d1d6c1c7af992b45b8aad7344671d89bf56
yash-nomadic <yashsingh4537@gmail.com>
2025-02-02 14:20:22 +0530

    pages

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,34 +1,40 @@
-import React from 'react'
+import React from 'react';
+import myclasses from './login.module.css';
 
 const Login = () => {
   return (
+    <section className='flex justify-center h-screen items-center'>
 
 
 
-    <div className='border w-1/5 border-gray-800 align center rounded-lg shadow-gray-800  '>
+      <div className='border w-1/5 border-gray-800 align center rounded-lg shadow-gray-800 shadow-xl  '>
 
-      <h1 className='text-center font-bold my-10 text-3xl'>Login</h1>
+        <h1 className='text-center font-bold my-10 text-3xl'>Login</h1>
 
-      <div>
+        <div>
 
-        <form className='text-xl p-8 '>
+          <form className='text-xl p-8  font-semibold  '>
 
-          <label htmlFor="email" >Email</label><br />
-          <input type="text" placeholder='Enter your email here' className='border border-gray-800 w-full ' /><br />
-
-
-          <label htmlFor="pasword">Password</label><br />
-          <input type="password" placeholder='Enter your password here' className='border border-gray-800 w-full ' /><br />
-
-          <button className='border w-1/2  rounded-lg bg-blue-500 text-white text-center'>Submit</button>
+            <label htmlFor="email" >Email</label>
+            <input type="text" placeholder='Enter your email here' className='border border-gray-800 w-full ' /><br />
 
 
-        </form>
+            <label htmlFor="pasword">Password</label>
+            <input type="password" placeholder='Enter your password here' className='border border-gray-800 w-full ' /><br />
 
+            <button className='border w-1/2  rounded-lg bg-blue-500 text-white text-center'>Submit</button>
+
+            <button className={myclasses.localBtn}>Local Button</button>
+
+
+          </form>
+
+
+        </div>
 
       </div>
 
-    </div>
+    </section>
 
 
 
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -3,45 +3,50 @@ import React from 'react'
 const Signup = () => {
   return (
 
-    <div className='border border-gray-800 w-1/2 p-6 rounded-xl'>
-      <h1 className='text-3xl text-center font-bold my-6  '>Register Your Details</h1>
+    <section  className='flex justify-center h-screen items-center '>
 
-      <div>
+      <div className='border border-gray-800 w-1/2 p-6 rounded-xl shadow-gray-800 shadow-xl '>
+        <h1 className='text-3xl text-center font-bold my-6  '>Register Your Details</h1>
 
-        <form className='text-xl'>
+        <div>
 
-          <label htmlFor="first_name">First Name</label><br />
-          <input type="text" placeholder='Enter your first name here' className='border border-gray-800 w-1/2 '/><br />
+          <form className='text-xl'>
 
-          <label htmlFor="last_name">Last Name</label><br />
-          <input type="text" placeholder='Enter your last name here' className='border border-gray-800 w-1/2' /><br />
+            <label htmlFor="first_name">First Name</label><br />
+            <input type="text" placeholder='Enter your first name here' className='border border-gray-800 w-1/2 ' /><br />
 
-          <label htmlFor="email">Email</label><br />  
-          <input type="text" placeholder='Enter your email ' className='border border-gray-800 w-1/2'/><br/>
+            <label htmlFor="last_name">Last Name</label><br />
+            <input type="text" placeholder='Enter your last name here' className='border border-gray-800 w-1/2' /><br />
 
-          <label htmlFor="text">Username</label><br />
-          <input type="text" placeholder='Create your username' className='border border-gray-800 w-1/2'/><br />
+            <label htmlFor="email">Email</label><br />
+            <input type="text" placeholder='Enter your email ' className='border border-gray-800 w-1/2' /><br />
 
-          <label htmlFor="password">Create Password</label><br />
-          <input type="password" placeholder='Create your password' className='border border-gray-800 w-1/2' /><br />
+            <label htmlFor="text">Username</label><br />
+            <input type="text" placeholder='Create your username' className='border border-gray-800 w-1/2' /><br />
 
-          <label htmlFor="password">Confirm Password</label><br />
-          <input type="password" placeholder='Confirm your password' className='border border-gray-800 w-1/2' /><br />
+            <label htmlFor="password">Create Password</label><br />
+            <input type="password" placeholder='Create your password' className='border border-gray-800 w-1/2' /><br />
 
-          <button className='border w-1/4  rounded-lg bg-blue-500 text-white text-center my-8 p-2'>Register</button>        
+            <label htmlFor="password">Confirm Password</label><br />
+            <input type="password" placeholder='Confirm your password' className='border border-gray-800 w-1/2' /><br />
+
+            <button className='border w-1/4  rounded-lg bg-blue-500 text-white text-center my-8 p-2'>Register</button>
 
 
-        </form>
+          </form>
+
+
+        </div>
+
 
 
       </div>
 
-       
 
-    </div> 
-    
+    </section>
 
-         
+
+
   )
 }
 
