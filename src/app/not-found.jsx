20dccb6f061969179f76c504925f99c664e7c8b8
yash-nomadic <yashsingh4537@gmail.com>
2025-02-03 13:46:46 +0530

    not found

---
+++ src/app/not-found.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className='flex h-screen justify-center items-center'>
+            <div className='text-center space-y-4'>
+                <h1 className='text-9xl font-bold'>404</h1>
+                <h2 className='text-9xl font-bold'>Not Found</h2>
+                <Link href='/' className='bg-black text-white rounded-xl p-3'>Back To Home</Link>
+            </div>
+        </div>
+
+    )
+}
+
+export default NotFound
