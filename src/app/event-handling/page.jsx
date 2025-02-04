7973d38888b09281212b20428f5e1c527df24860
yash-nomadic <yashsingh4537@gmail.com>
2025-02-04 13:09:58 +0530

    event handling

---
+++ src/app/event-handling/page.jsx
@@ -0,0 +1,22 @@
+'use client';
+
+import React from 'react'
+
+const EventHandling = () => {
+    return (
+        <div className='max-w-[80%] mx-auto'>
+
+            <h1 className='text-3xl font-bold text-center my-6'>Event Handling</h1>
+            <hr />
+
+            <button
+                onClick={ () => { aler('button was clicked'); }  }
+                className='border rounded-md p-3 bg-gray-200'>
+                Click me
+            </button>
+
+        </div>
+    )
+}
+
+export default EventHandling
