c78015ea71de71aa5d9f28de4f565ad8c21d7615
yash-nomadic <yashsingh4537@gmail.com>
2025-02-06 13:59:09 +0530

    chatbox

---
+++ src/app/chatbox/page.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Chat = () => {
+  return (
+    <div className='bg-slate-200 h-screen'>
+
+        <div>
+
+            <div>
+                
+                <input 
+                type="text" />
+            
+            </div>
+
+            <div>
+                <h1>CHAT BOX</h1>
+           
+            </div>
+        
+        </div>
+    
+    </div>
+  )
+}
+
+export default Chat
