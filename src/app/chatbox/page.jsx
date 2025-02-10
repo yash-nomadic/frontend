f4dc27ff46f9827e06c822e9cddd60a551693ffb
yash-nomadic <yashsingh4537@gmail.com>
2025-02-10 13:22:12 +0530

    frontend signup page

--- src/app/chatbox/page.jsx
+++ src/app/chatbox/page.jsx
@@ -1,27 +1,76 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 
 const Chat = () => {
-  return (
-    <div className='bg-slate-200 h-screen'>
 
-        <div>
+    const [msgList, setMsgList] = useState();
 
-            <div>
-                
-                <input 
-                type="text" />
-            
+
+    const addMsg = (e) => {
+
+        if (e.code === 'Enter') {
+
+            if (!e.target.value.trim()) {
+                return alert('Please Type Something')
+            }
+
+            console.log(e.target.value);
+
+            const newMsg = { text: e.target.value, complete: false }
+
+            setMsgList([addMsg, ...msgList]);
+
+            e.target.value = '';
+
+        }
+
+    }
+
+
+
+
+
+    return (
+        <div className='bg-slate-200 h-screen'>
+
+            <div className='mx-auto py-10 max-w-[80%]'>
+
+                <div className='bg-white rounded-lg shadow-lg   '>
+
+                    <input
+                        onKeyDown={addMsg}
+                        className='w-full p-6 border-b-2 '
+                        type="text" />
+
+                </div>
+
+                <div className='p-8 h-[60vh] overflow-y-scroll'>
+                    {
+                        msgList.map((msg, index) => {
+                            return <div key={index} className='shadow mb-5 rounded-lg p-5 bg-blue-500 font-medium text-white flex justify-between items-center'>
+                                <p>{msg.text}</p>
+                            </div>
+                        })
+
+
+                    }
+
+
+
+
+                </div>
+
+                <div className=' '>
+                    <h1 className='text-3xl font-bold text-center '>CHAT BOX</h1>
+
+                </div>
+
             </div>
 
-            <div>
-                <h1>CHAT BOX</h1>
-           
-            </div>
-        
         </div>
-    
-    </div>
-  )
+    )
+
 }
 
 export default Chat
