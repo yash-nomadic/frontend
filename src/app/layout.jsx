471d7a495126cc83014573b5de35a0010726d376
yash-nomadic <yashsingh4537@gmail.com>
2025-02-18 22:14:19 +0530

    layout page jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
          
         <Navbar/>
+        <Toaster position="top-right" />
         {children}
         <Footer/>
       </body>
