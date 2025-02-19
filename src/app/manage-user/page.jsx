cc8a0e2c699d69bb5fd9324492c23607d28d37b3
yash-nomadic <yashsingh4537@gmail.com>
2025-02-19 23:06:38 +0530

    manage user page

--- src/app/manage-user/page.jsx
+++ src/app/manage-user/page.jsx
@@ -1,30 +1,79 @@
-'use client'
-
+'use client';
+import { IconTrash } from '@tabler/icons-react';
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { use, useEffect, useState } from 'react'
+import toast from 'react-hot-toast';
 
 const ManageUser = () => {
 
-    const fetchUserData = async () => {
 
-       const res = await axios.get('http://localhost:5000/user/getall');
-       console.log(res.data);
-       
+    const [userList, setUserList] = useState([]);
+
+    const fetchUserData = async () => {
+        const res = await axios.get('http://localhost:5000/user/getall');
+        console.table(res.data);
+        setUserList(res.data);
     }
 
     useEffect(() => {
         fetchUserData();
-    },[]);
+    }, []);
+
+    const deleteUser = (id) => {
+        axios.delete(`http://localhost:5000/user/delete/ ${id}`)
+            .then((result) => {
+                toast.success('User deleted successfully');
+                fetchUserData();
+            }).catch((err) => {
+                console.log(err);
+                toast.error('Falied to delete user');
+
+            });
+    }
 
     return (
-
         <div>
-
             <div className='max-w-[80%] mx-auto h-screen mt-8'>
-                <h1 className='text-4xl font-bold text-center'>Manage User</h1>
+                <h1 className='text-3xl font-bold text-center'>ManageUser</h1>
+
+                <table className='w-full mt-10 border-2 border-blue-300'>
+                    <thead className='bg-blue-600 text-white'>
+                        <tr>
+                            <th>S.No</th>
+                            <th>ID</th>
+                            <th>NAME</th>
+                            <th>EMAIL</th>
+                            <th>CITY</th>
+                            <th>DATE</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            userList.map((user, index) => {
+                                return <tr key={user._id}
+                                    className= {`border-2 border-blue-300 ${index % 2 === 0 ? 'bg-blue-100' : 'bg-blue-200'}`}>
+                                    {/* className={'border-2 border-blue-300 ' + (index % 2 === 0 ? 'bg-blue-100' : 'bg-blue-200')}> */}
+                                    <td className='p-3'>{index + 1}</td>
+                                    <td className='p-3'>{user._id}</td>
+                                    <td className='p-3'>{user.name}</td>
+                                    <td className='p-3'>{user.email}</td>
+                                    <td className='p-3'>{user.city}</td>
+                                    <td className='p-3'>{new Date(user.createdAt).toLocaleDateString()}</td>
+
+                                    <td>
+                                        <button onClick={() => { deleteUser(user._id) }}
+                                            className='rounded bg-red-500 text-white px-3 py-1'>
+                                            <IconTrash />
+                                        </button>
+                                    </td>
+                                </tr>
+                            })
+                        }
+                    </tbody>
+                </table>
 
             </div>
-
         </div>
     )
 }
